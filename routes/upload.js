bf4d024bb91f7496584e4d127b8e17797e64941e
trung.luong@apps-cyclone.com <trung.luong@apps-cyclone.com>
2018-02-12 16:25:23 +0700

    upload file with type of file

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
+var crypto = require('crypto');
 
-var upload = multer({dest: "./public/uploads"}).single('photo');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/uploads/')
+    },
+    filename: function (req, file, cb) {
+    
+      crypto.pseudoRandomBytes(16, function (err, raw) {
+        cb(null, raw.toString('hex') + Date.now() + '.' + file.originalname.split(".")[file.originalname.split(".").length - 1]);
+      });
+    }
+  });
+  var upload = multer({ storage: storage }).single('uploadfile');
 
 
 router.post('/', function (req, res, next) {
@@ -15,11 +27,11 @@ router.post('/', function (req, res, next) {
          return res.status(422).send("an Error occured")
        }  
       // No error occured.
-       path = req.file.path;
-       console.log(req.file)
+       path = req.file.filename;
+       
        return res.send({
            ok:true,
-           url:path
+           url:"/uploads/" + path
         }); 
  });     
 })
